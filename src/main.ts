e7082ad34635853c68c7f1e3eaed2d7e58967a78
Mohana Misra <iammohanamisra@gmail.com>
2025-04-07 18:22:17 +0530

    Adding frontend url in production and dev mode to CORS

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'https://quotation-module-frontend.vercel.app',
+    ],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
